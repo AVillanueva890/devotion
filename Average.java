dceeb38bf997e1a404f765bd662bd7f0a8db9ae7
AVillanueva890 <117901610+AVillanueva890@users.noreply.github.com>
2022-11-10 17:53:19 +0800

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Average {
+	public static void main(String[] args) {
+		Scanner Iskor=new Scanner(System.in);
+		int Grado=1;
+		int total=0;
+		int rep=0;
+		
+		while(Grado==1){
+		    System.out.println("Enter Grade:");
+		    int grado=Iskor.nextInt();
+		    
+		    total+=Grado;
+		    rep+=Grado;
+		    
+		    System.out.println("Do you want to enter another grade?\n[1]YES   [2]NO:");
+		    Grado=Iskor.nextInt();
+		    
+		}
+		total=total/rep;
+		System.out.println("AVERAGE:"+total);
+    }	
+}
